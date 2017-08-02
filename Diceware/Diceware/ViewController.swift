fe62624104cef5f492eca05894cb0a522554be77
Steven Huey <stevenhuey@gmail.com>
2017-08-02 10:27:09 -0400

    Clean up the project and boilerplate

--- Diceware/Diceware/ViewController.swift
+++ Diceware/Diceware/ViewController.swift
@@ -2,24 +2,11 @@
 //  ViewController.swift
 //  Diceware
 //
-//  Created by Steve Huey on 8/2/17.
 //  Copyright © 2017 Steve Huey. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      // Do any additional setup after loading the view, typically from a nib.
-   }
-
-   override func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
-   }
-
-
 }
 
